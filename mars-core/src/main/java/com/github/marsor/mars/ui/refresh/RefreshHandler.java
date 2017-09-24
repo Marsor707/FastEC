870ca37eb7ad3edcb03c8b9e396535eada345780
marsor <369135912@qq.com>
2017-09-24 16:50:30 +0800

    完成首页界面显示

--- mars-core/src/main/java/com/github/marsor/mars/ui/refresh/RefreshHandler.java
+++ mars-core/src/main/java/com/github/marsor/mars/ui/refresh/RefreshHandler.java
@@ -1,11 +1,17 @@
 package com.github.marsor.mars.ui.refresh;
 
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.RecyclerView;
 import android.widget.Toast;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.github.marsor.mars.app.Mars;
 import com.github.marsor.mars.net.RestClient;
 import com.github.marsor.mars.net.callback.ISuccess;
+import com.github.marsor.mars.ui.recycler.DataConverter;
+import com.github.marsor.mars.ui.recycler.MultipleRecyclerAdapter;
 
 /**
  * Author: Marsor
@@ -13,13 +19,27 @@ import com.github.marsor.mars.net.callback.ISuccess;
  * Email: 369135912@qq.com
  */
 
-public class RefreshHandler implements SwipeRefreshLayout.OnRefreshListener {
+public class RefreshHandler implements SwipeRefreshLayout.OnRefreshListener,
+        BaseQuickAdapter.RequestLoadMoreListener {
 
     private final SwipeRefreshLayout REFRESH_LAYOUT;
+    private final PagingBean BEAN;
+    private final RecyclerView RECYCLERVIEW;
+    private MultipleRecyclerAdapter mAdapter = null;
+    private final DataConverter CONVERTER;
 
-    public RefreshHandler(SwipeRefreshLayout refresh_layout) {
+    private RefreshHandler(SwipeRefreshLayout refresh_layout, RecyclerView recyclerView,
+                           DataConverter converter, PagingBean bean) {
         this.REFRESH_LAYOUT = refresh_layout;
+        this.RECYCLERVIEW = recyclerView;
+        this.CONVERTER = converter;
+        this.BEAN = bean;
         REFRESH_LAYOUT.setOnRefreshListener(this);
+    }
+
+    public static RefreshHandler create(SwipeRefreshLayout refresh_layout, RecyclerView recyclerView,
+                                        DataConverter converter) {
+        return new RefreshHandler(refresh_layout, recyclerView, converter, new PagingBean());
     }
 
     private void refresh() {
@@ -33,11 +53,20 @@ public class RefreshHandler implements SwipeRefreshLayout.OnRefreshListener {
     }
 
     public void firstPage(String url) {
+        BEAN.setDelayed(1000);
         RestClient.builder()
                 .url(url)
                 .success(new ISuccess() {
                     @Override
                     public void onSuccess(String response) {
+                        final JSONObject object = JSON.parseObject(response);
+                        BEAN.setTotal(object.getInteger("total"))
+                                .setPageSize(object.getInteger("page_size"));
+                        //设置Adapter
+                        mAdapter = MultipleRecyclerAdapter.create(CONVERTER.setJsonData(response));
+                        mAdapter.setOnLoadMoreListener(RefreshHandler.this, RECYCLERVIEW);
+                        RECYCLERVIEW.setAdapter(mAdapter);
+                        BEAN.addIndex();
                     }
                 })
                 .build()
@@ -47,5 +76,10 @@ public class RefreshHandler implements SwipeRefreshLayout.OnRefreshListener {
     @Override
     public void onRefresh() {
         refresh();
+    }
+
+    @Override
+    public void onLoadMoreRequested() {
+
     }
 }
